4238d78c63b442a9133e1a515ff588a955f23e1e
Binaya Bhattarai <bhattaraibinayar@gmail.com>
2017-09-11 17:50:46 -0500

    switch views to class-based views

--- restaurants/views.py
+++ restaurants/views.py
@@ -2,24 +2,25 @@ import random
 
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+class HomeView(TemplateView):
+	template_name = "home.html"
+		
+	def get_context_data(self, *args, **kwargs):
+		#context = super(HomeView, self).get_context_data(*args, **kwargs)
+		l = [random.randint(1,100) for i in range(5)]
+		context = {
+			"the_list":l, 
+			"bool_var": True, 
+			"rand_num": random.randint(1, 100)
+		}
+		return context
+		
+class AboutView(TemplateView):
+	template_name = "about.html"
 
-def home(request):
-	l = [random.randint(1,100) for i in range(5)]
-	context = {
-		"the_list":l, 
-		"bool_var": True, 
-		"rand_num": random.randint(1, 100)
-	}
-	return render(request, "home.html", context)   #response
+class ContactView(TemplateView):
+	template_name = "contacts.html"
 
-def about(request):
-	context = {
-	}
-	return render(request, "about.html", context)   #response
-
-def contacts(request):
-	context = {
-	}
-	return render(request, "contacts.html", context)   #response
