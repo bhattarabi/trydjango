2f707df1039d254255958a276b84b65ceb4fc9ae
Binaya Bhattarai <bhattaraibinayar@gmail.com>
2017-09-12 15:23:48 -0500

    add urls for restaurant list and detail views

--- trydjango/urls.py
+++ trydjango/urls.py
@@ -15,12 +15,16 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
-from restaurants.views import HomeView, AboutView, ContactView
+from restaurants.views import RestaurantListView, RestaurantDetailView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', HomeView.as_view()),
-    url(r'^about/$', AboutView.as_view()),
-    url(r'^contact/$', ContactView.as_view()),
+    url(r'^restaurants/$', RestaurantListView.as_view()),
+    #url(r'^restaurants/(?P<category>\w+)/$', RestaurantListView.as_view()),
+    url(r'^restaurants/(?P<pk>\w+)/$', RestaurantDetailView.as_view()),
+    url(r'^$', TemplateView.as_view(template_name="home.html")),
+    url(r'^about/$', TemplateView.as_view(template_name="about.html")),
+    url(r'^contact/$', TemplateView.as_view(template_name="contacts.html")),
 ]
