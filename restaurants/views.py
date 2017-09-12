5118da0b2e4066192a164300b3a2344e1816a1aa
Binaya Bhattarai <bhattaraibinayar@gmail.com>
2017-09-12 15:21:12 -0500

    create list and detail views for RestaurantLocation

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,26 +1,25 @@
-import random
-
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
 
-# Create your views here.
-class HomeView(TemplateView):
-	template_name = "home.html"
-		
-	def get_context_data(self, *args, **kwargs):
-		#context = super(HomeView, self).get_context_data(*args, **kwargs)
-		l = [random.randint(1,100) for i in range(5)]
-		context = {
-			"the_list":l, 
-			"bool_var": True, 
-			"rand_num": random.randint(1, 100)
-		}
-		return context
-		
-class AboutView(TemplateView):
-	template_name = "about.html"
+from .models import RestaurantLocation
 
-class ContactView(TemplateView):
-	template_name = "contacts.html"
+class RestaurantListView(ListView):
+    #template_name = "restaurants/restaurants_list.html"
+    def get_queryset(self):
+        slug = self.kwargs.get("category")
+        if slug:
+            queryset = RestaurantLocation.objects.filter(category__iexact=slug)
+        else:
+            queryset = RestaurantLocation.objects.all()
+        
+        return queryset
+    
+    
+class RestaurantDetailView(DetailView):
+    queryset = RestaurantLocation.objects.all()
+    
+    def get_context_data(self, *args, **kwargs):
+        context = super(RestaurantDetailView, self).get_context_data(*args, **kwargs)
+        return context
 
