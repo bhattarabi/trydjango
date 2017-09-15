43134a7f1c48090a5fad6ddf485076d4ecc7f751
Binaya Bhattarai <bhattaraibinayar@gmail.com>
2017-09-15 10:30:10 -0500

    add slug field and title property

--- restaurants/models.py
+++ restaurants/models.py
@@ -3,11 +3,16 @@ from django.db import models
 # Create your models here.
 
 class RestaurantLocation(models.Model):
-    name = models.CharField(max_length=120)
-    location = models.CharField(max_length=120, null=True, blank=True)
-    category = models.CharField(max_length=120, null=True, blank=True)
-    timestamp = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    name        = models.CharField(max_length=120)
+    location    = models.CharField(max_length=120, null=True, blank=True)
+    category    = models.CharField(max_length=120, null=True, blank=True)
+    timestamp   = models.DateTimeField(auto_now_add=True)
+    updated     = models.DateTimeField(auto_now=True)
+    slug        = models.SlugField(null=True, blank=True)
 
     def __str__(self):
+        return self.name
+    
+    @property
+    def title(self):
         return self.name
