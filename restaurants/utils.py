038f63a15a3059b36dbca4b544657192304f2495
Binaya Bhattarai <bhattaraibinayar@gmail.com>
2017-09-15 10:30:29 -0500

    first commit

---
+++ restaurants/utils.py
@@ -0,0 +1,30 @@
+from django.utils.text import slugify
+
+import random
+import string
+
+
+def generate_random_string(size=10, 
+            chars=string.ascii.lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+
+def generate_unique_slug(instance, new_slug=None):
+    
+    if new_slug is not None:
+        slug = new_slug
+    else:
+        slug = slugify(instance.title)
+        
+    Klass = instance.__class__
+    qs_exists = Klass.objects.filter(slug=slug).exists()
+    
+    if qs_exists:
+        new_slug = "{slug}-{randstr}".format(
+                        slug=slug,
+                        randstr=generate_random_string(size=4)
+                    )
+        
+        return generate_unique_slug(instance, new_slug=new_slug)
+    return slug
