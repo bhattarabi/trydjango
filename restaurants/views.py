55742bbbf7c16d328ba87e11b94e11959d0ae4c6
Binaya Bhattarai <bhattaraibinayar@gmail.com>
2017-09-15 11:41:30 -0500

    update detail view to go off of slugs

--- restaurants/views.py
+++ restaurants/views.py
@@ -19,12 +19,9 @@ class RestaurantListView(ListView):
 class RestaurantDetailView(DetailView):
     queryset = RestaurantLocation.objects.all()
     
+    '''
     def get_object(self, *args, **kwargs):
         rest_id = self.kwargs.get('rest_id')
         obj = get_object_or_404(RestaurantLocation, id=rest_id)
         return obj
-    
-    def get_context_data(self, *args, **kwargs):
-        context = super(RestaurantDetailView, self).get_context_data(*args, **kwargs)
-        return context
-
+        '''
