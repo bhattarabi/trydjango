86fdc5a71c4ed51b53eb55d290d623e179ede1a2
Binaya Bhattarai <bhattaraibinayar@gmail.com>
2017-09-15 11:43:00 -0500

    update detail view to go off of slugs

--- trydjango/urls.py
+++ trydjango/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^restaurants/$', RestaurantListView.as_view()),
     #url(r'^restaurants/(?P<category>\w+)/$', RestaurantListView.as_view()),
-    url(r'^restaurants/(?P<pk>\w+)/$', RestaurantDetailView.as_view()),
+    url(r'^restaurants/(?P<slug>[\w-]+)/$', RestaurantDetailView.as_view()),
     url(r'^$', TemplateView.as_view(template_name="home.html")),
     url(r'^about/$', TemplateView.as_view(template_name="about.html")),
     url(r'^contact/$', TemplateView.as_view(template_name="contacts.html")),
