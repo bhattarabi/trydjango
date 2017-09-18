9ba6210c6a42a09cc7f57bba9274eb879d00b04a
Binaya Bhattarai <bhattaraibinayar@gmail.com>
2017-09-18 14:12:05 -0500

    implement DONT_USE slugs

--- restaurants/utils.py
+++ restaurants/utils.py
@@ -4,6 +4,8 @@ import random
 import string
 
 
+DONT_USE = ['create']
+
 def generate_random_string(size=10, 
             chars=string.ascii_lowercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
@@ -16,6 +18,13 @@ def generate_unique_slug(instance, new_slug=None):
         slug = new_slug
     else:
         slug = slugify(instance.title)
+        
+    if slug in DONT_USE:
+        new_slug = "{slug}-{randstr}".format(
+                        slug=slug,
+                        randstr=generate_random_string(size=4)
+                    )
+        return generate_unique_slug(instance, new_slug=new_slug)
         
     Klass = instance.__class__
     qs_exists = Klass.objects.filter(slug=slug).exists()
